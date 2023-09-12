2bfdeb04a8e71577ebad53a6d4f7fcc14e948826
Dairop <biagi.dorian49@gmail.com>
2023-09-12 15:24:42 +0200

    Base de faits

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Element e1 = new Element("nomFun", true);
+        Element e2 = new Element("nomMoinsFunl", false);
+
+        BaseDeFaits b1 = new BaseDeFaits();
+        b1.ajouterFait(e1);
+        b1.ajouterFait(e2);
+
+        System.out.println(b1.toString());
+
+        b1.retireFait(e2);
+        System.out.println(b1.toString());
+
     }
 }
